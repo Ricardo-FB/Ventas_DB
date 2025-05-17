10d9b001bd3a25ace2a3b35a54846683fd4eb08f
Ricardo-FB <ricardobenitez1999.rjfb@gmail.com>
2025-05-17 19:14:54 -0300

    "ddl_ventas.sql"

--- ddl_ventas.sql
+++ ddl_ventas.sql
@@ -1,6 +1,3 @@
--- Crear la base de datos
-CREATE DATABASE ventas_db;
-
 -- Crear la tabla Clientes
 CREATE TABLE Clientes (
     ID_Cliente SERIAL PRIMARY KEY,
@@ -19,18 +16,18 @@ CREATE TABLE Metodos_Pago (
 
 -- Crear la tabla Ventas
 CREATE TABLE Ventas (
-    id_venta SERIAL PRIMARY KEY,
-    fecha DATE NOT NULL DEFAULT CURRENT_DATE
+    ID_venta SERIAL PRIMARY KEY,
+    fecha DATE NOT NULL DEFAULT CURRENT_DATE,
     Hora TIME,
-    id_cliente SERIAL,
-    ID_metodo_pago SERIAL,
-    FOREIGN KEY (id_cliente) REFERENCES Clientes(ID_Cliente),
-    FOREIGN KEY (ID_metodo_pago) REFERENCES Metodos_Pago(ID_metodo_pago)
+    ID_cliente INTEGER,
+    ID_metodo_pago INTEGER,
+    FOREIGN KEY (ID_cliente) REFERENCES Clientes(ID_Cliente) ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (ID_metodo_pago) REFERENCES Metodos_Pago(ID_metodo_pago) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 -- Crear la tabla Productos
 CREATE TABLE Productos (
-    Id_Producto SERIAL PRIMARY KEY,
+    ID_Producto SERIAL PRIMARY KEY,
     Nombre VARCHAR(100),
     Uni_medida VARCHAR(10),
     Precio_Venta DECIMAL(10, 2)
@@ -38,27 +35,27 @@ CREATE TABLE Productos (
 
 -- Crear la tabla Detalle_Ventas
 CREATE TABLE Detalle_Ventas (
-    id_venta SERIAL,
-    id_producto SERIAL,
-    Cantidad SERIAL,
+    ID_venta INTEGER,
+    ID_producto INTEGER,
+    Cantidad INT,
     Precio_unitario DECIMAL(10, 2),
     Subtotal DECIMAL(10, 2),
-    PRIMARY KEY (id_venta, id_producto), 
-    FOREIGN KEY (id_venta) REFERENCES Ventas(id_venta),
-    FOREIGN KEY (id_producto) REFERENCES Productos(Id_Producto)
+    PRIMARY KEY (ID_venta, ID_producto), 
+    FOREIGN KEY (ID_venta) REFERENCES Ventas(ID_venta) ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (ID_producto) REFERENCES Productos(ID_Producto) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 -- Crear la tabla Facturas
 CREATE TABLE Facturas (
     ID_Factura SERIAL PRIMARY KEY,
+    ID_cliente INTEGER,
+    ID_venta INTEGER,
     Timbrado VARCHAR(8),
-    ID_venta SERIAL,
     num_Factura VARCHAR(15),
-    ID_cliente SERIAL,
     fecha_emision DATE,
     Subtotal DECIMAL(10, 2),
     IVA DECIMAL(10, 2),
     Total DECIMAL(10, 2),
-    FOREIGN KEY (ID_venta) REFERENCES Ventas(id_venta),
-    FOREIGN KEY (ID_cliente) REFERENCES Clientes(ID_Cliente)
+    FOREIGN KEY (id_venta) REFERENCES Ventas(id_venta) ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id_Cliente) ON UPDATE CASCADE ON DELETE RESTRICT
 );
