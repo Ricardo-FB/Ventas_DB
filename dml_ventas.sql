06bc3644ac527b47c42dd7c09b848506b80bdadd
Ricardo-FB <ricardobenitez1999.rjfb@gmail.com>
2025-05-17 19:54:23 -0300

    "dml_ventas.sql"

--- dml_ventas.sql
+++ dml_ventas.sql
@@ -1,26 +1,32 @@
-INSERT INTO Clientes (nombre, apellido, telefono, direccion) VALUES
-('Juan', 'Perez', '0981123456', 'Calle 1 Nro 123'),
-('Maria', 'Gomez', '0994789012', 'Avenida 2 Nro 456'),
-('Carlos', 'Lopez', NULL, 'Barrio Centro');
-
-
-INSERT INTO 
-Productos (nombre, descripcion, precio_venta, stock) 
+INSERT INTO Clientes (id_cliente, nombres, telefono, direccion) 
 VALUES
-('Azucar Blanco', 'Azucar a granel kg.', 8000.00, 150),
-('Carne cortada', 'carne de primera cortada', 56000.00, 50),
-('Fideo talllarin', 'Paquete de fideos 400GR.', 6000.00, 30),
-('Cinta de Raso Roja', 'Rollo de cinta de raso de 2cm de ancho', 4000.00, 80);
+(1, 'Juan Perez', '0981123456', 'Calle 1 Nro 123'),
+(2, 'Maria Gomez', '0994789012', 'Avenida 2 Nro 456'),
+(3, 'Carlos Lopez', NULL, 'Barrio Centro');
 
 
 INSERT INTO 
-Ventas (cliente_id, total_venta) 
+Productos (id_producto, nombre, precio_venta, stock) 
 VALUES
-(1, 78000.00);
+(1, 'Azucar Blanco', 8000.00, 150),
+(2, 'Carne cortada', 56000.00, 50),
+(3, 'Fideo talllarin', 6000.00, 30),
+(4, 'Cinta de Raso Roja', 4000.00, 80);
+
+INSERT INTO
+Metodos_Pago(id_metodo_pago, descripcion)
+VALUES 
+(1, 'Contado'),
+(2, 'Credito');
+
+INSERT INTO 
+Ventas (id_venta, ID_cliente, fecha, id_metodo_pago, hora) 
+VALUES
+(1, 1, '2025-05-17', 1, '10:45:22');
 
 
 INSERT INTO 
-DetalleVenta (venta_id, producto_id, cantidad, precio_unitario, subtotal) 
+Detalle_Ventas (ID_venta, ID_producto, cantidad, precio_unitario, subtotal) 
 VALUES  (1, 1, 2, 8000.00, 16000.00), 
         (1, 3, 1, 6000.00, 6000.00),  
         (1, 2, 1, 56000.00, 56000.00);  
